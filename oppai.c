237aa1f8c6890214b2d548fbe515751ab55b3b05
James <james@shibui.pw>
2021-02-01 18:49:34 +0000

    oops

--- oppai.c
+++ oppai.c
@@ -2215,7 +2215,7 @@ int pp_std(ezpp_t ez) {
       /*if (ez->bpm > 330) {
         ez->pp *= 1.02f;
       }*/
-      if (ez->ar > 10.9 && ez->accuracy >= 99.00) {
+      if (ez->ar > 10.9 && accuracy >= 99.00) {
         ez->pp *= 1.02f;
       }
   	  switch (ez->beatmap_id) {
